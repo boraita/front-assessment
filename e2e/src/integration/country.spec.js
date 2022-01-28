99f841c015365364922f5912f8ef2c25f75ffbbe
Rafael Montano Boraita <Rafael.MontanoBoraita@liberty-it.co.uk>
2022-01-28 10:37:31 +0100

    Improve test for e2e

--- e2e/src/integration/country.spec.js
+++ e2e/src/integration/country.spec.js
@@ -1,7 +1,9 @@
 describe('Country', () => {
   beforeEach(() => {
-    cy.intercept('**/country/city', { fixture: 'country-city.json' })
-      .as('loadCounties');
+    if (Cypress.env('env') !== 'ci') {
+      cy.intercept('**/country/city', { fixture: 'country-city.json' })
+        .as('loadCounties');
+    }
   });
 
   it('opens the app', () => {
@@ -11,9 +13,11 @@ describe('Country', () => {
     cy.get('.capital-container').get('.select-country').contains('Select a country');
   });
   it('should select first country and display it', () => {
-    cy.get('a.btn').then(countries => countries[0].click());
-    cy.get('.capital-container').not('.select-country');
-    cy.get('.country-name').contains('Afghanistan');
-    cy.get('.city-name').contains('Kabul');
+    cy.get('a.btn').then(countries => {
+      const randomNumber = Math.floor(Math.random() * countries.length);
+      countries[randomNumber].click();
+      cy.get('.capital-container').not('.select-country');
+      cy.get('.country-name').contains(countries[randomNumber].innerText);
+    });
   });
 });
